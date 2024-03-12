e2ea609130333700ed2324efd86f8e768b116888
zalayetha <zalayetha@gmail.com>
2024-03-12 21:50:21 +0700

    Setup Retrofit and response data class for each endpoint

---
+++ app/src/main/java/com/zaghy/githubuser/data/retrofit/ApiService.kt
@@ -0,0 +1,23 @@
+package com.zaghy.githubuser.data.retrofit
+
+import com.zaghy.githubuser.data.response.UserDetailResponse
+import com.zaghy.githubuser.data.response.UserFollowersResponse
+import com.zaghy.githubuser.data.response.UserFollowingResponse
+import com.zaghy.githubuser.data.response.UserListResponse
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+
+    @GET("search/users")
+    fun getUserByWord(@Query("q") user:String):Call<UserListResponse>
+
+    @GET("users/{username}")
+    fun getDetailUser(@Path("username") username:String):Call<UserDetailResponse>
+
+    @GET("users/{username}/followers")
+    fun getFollowersUser(@Path("username") username: String):Call<UserFollowersResponse>
+
+    @GET("users/{username}/following")
+    fun getFollowingUser(@Path("username") username:String):Call<UserFollowingResponse>
+}
