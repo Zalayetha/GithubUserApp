037f29dd7a96903b7af5264c54994543a113d02c
zalayetha <zalayetha@gmail.com>
2024-03-13 22:08:59 +0700

    successfull show data user

--- app/src/main/java/com/zaghy/githubuser/view/MainActivity.kt
+++ app/src/main/java/com/zaghy/githubuser/view/MainActivity.kt
@@ -2,20 +2,28 @@ package com.zaghy.githubuser.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.annotation.StringRes
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.zaghy.githubuser.R
+import com.zaghy.githubuser.data.response.ItemsItem
 import com.zaghy.githubuser.databinding.ActivityMainBinding
+import com.zaghy.githubuser.view.adapter.RecyclerViewAdapter
+import com.zaghy.githubuser.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
-
-
+    private lateinit var  recyclerView:RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val viewModel = ViewModelProvider(this,ViewModelProvider.NewInstanceFactory())[MainViewModel::class.java]
         with(binding){
             searchView.setupWithSearchBar(searchBar)
             searchView.editText.setOnEditorActionListener { textview, actionId, event ->
@@ -25,5 +33,28 @@ class MainActivity : AppCompatActivity() {
                 false
             }
         }
+
+        recyclerView = binding.recyclerViewuser
+        recyclerView.setHasFixedSize(true)
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+
+        viewModel.userListItems.observe(this){user->
+            setUserList(user)
+        }
+        viewModel.isLoading.observe(this) {
+            showLoading(it)
+        }
+
     }
+
+    private fun setUserList(user: List<ItemsItem>?){
+        val adapter = RecyclerViewAdapter()
+        adapter.submitList(user)
+        binding.recyclerViewuser.adapter = adapter
+    }
+    fun showLoading(isLoading:Boolean){
+        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+    }
+
 }
--- app/src/main/java/com/zaghy/githubuser/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/zaghy/githubuser/viewmodel/MainViewModel.kt
@@ -1,7 +1,51 @@
 package com.zaghy.githubuser.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.zaghy.githubuser.data.response.ItemsItem
+import com.zaghy.githubuser.data.response.UserListResponse
+import com.zaghy.githubuser.data.retrofit.ApiConfig
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainViewModel : ViewModel(){
-    
+    companion object{
+        private const val TAG = "MainViewModel"
+    }
+
+    private val _userListItems = MutableLiveData<List<ItemsItem>>()
+    val userListItems:LiveData<List<ItemsItem>> = _userListItems
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    init {
+        _isLoading.value = true
+        findUserGithub(userName = "sidiq")
+    }
+    private fun findUserGithub(userName: String = "a"){
+         _isLoading.value = true
+        val client = ApiConfig.getApiService().getUserByWord(userName)
+        client.enqueue(object: Callback<UserListResponse>{
+            override fun onResponse(
+                call: Call<UserListResponse>,
+                response: Response<UserListResponse>
+            ) {
+                _isLoading.value = false
+                if(response.isSuccessful){
+                    _userListItems.value = response.body()?.items
+                }else{
+                    Log.e(TAG,"onFailure ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<UserListResponse>, t: Throwable) {
+                Log.e(TAG,"OnFailure ${t.message.toString()}")
+            }
+
+        })
+    }
 }
